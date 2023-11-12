b8d14eb2be63c561003b4d9ccdf98757b52d7d87
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-11-12 17:36:24 +0900

    이력서 정보 조회 API 구현

--- server/src/dao/indexDao.js
+++ server/src/dao/indexDao.js
@@ -1,10 +1,14 @@
 const { pool } = require("../../config/database");
 
-exports.exampleDao = async function (connection, params) {
-  const Query = ``;
-  const Params = [];
+/* resumeId로 회원의 이름, 주소, 휴대폰번호, 이메일 조회 */
+exports.selectUserByResumeId = async function (connection, resumeId) {
+  const selectUserByResumeIdQuery = 
+    `SELECT a.title, b.name, b.address, b.phoneNumber, b.email, a.profileImageUrl, a.introduce 
+      FROM Resumes a LEFT OUTER JOIN Users b ON a.userId=b.userId
+      WHERE a.resumeId = ?`;
+      const params = [ resumeId ];
 
-  const rows = await connection.query(Query, Params);
+      const rows = await connection.query(selectUserByResumeIdQuery, params);
 
-  return rows;
+      return rows;
 };
--- server/src/routes/indexRoute.js
+++ server/src/routes/indexRoute.js
@@ -2,4 +2,5 @@ const index = require("../controllers/indexController");
 
 module.exports = function (app) {
     app.get('/', index.greeting);
+    app.get('/resume/:resumeId', index.readResumeInfoByResumeId);
 };
