29472cd026c733c1ede22fe429fc779618e421ab
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-11-19 17:39:54 +0900

    node.js 서버 무중단 배포, 클라이언트 프로젝트 storybook, styledcomponent 설치, globalStyle 적용, Font 적용

---
+++ client/src/styles/global.ts
@@ -0,0 +1,87 @@
+import {createGlobalStyle} from "styled-components";
+import NotoSansKRRegular from '../assets/fonts/NotoSansKR-Regular.ttf';
+import NotoSansKRBold from "../assets/fonts/NotoSansKR-Bold.ttf";
+import NotoSansKRThin from "../assets/fonts/NotoSansKR-Thin.ttf";
+import PoorStoryRegular from "../assets/fonts/PoorStory-Regular.ttf";
+import tokens from '../styles/tokens.json';
+
+const globalTokens = tokens.global;
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family : 'NotoSansKR';
+    font-style : normal;
+    font-weight: normal;
+    src: url("${NotoSansKRRegular}");
+  }
+  @font-face {
+    font-family: 'NotoSansKR';
+    font-style: normal;
+    font-weight: bold;
+    src: url("${NotoSansKRBold}");
+  }
+  @font-face {
+    font-family: 'NotoSansKR';
+    font-style: normal;
+    font-weight: 300;
+    src: url("${NotoSansKRThin}");
+  }
+  @font-face {
+    font-family: 'PoorStoryRegular';
+    font-weight: 400;
+    src: url("${PoorStoryRegular}");
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'NotoSansKR';
+    font-weight: normal;
+    color: ${globalTokens.Black.value};
+    transition: 200ms;
+    outline: 0;
+    border: none;
+  }
+  button {
+    border-style: none;
+    background: none;
+    cursor: pointer;
+  }
+  ol,ul {
+    list-style: none;
+  }
+  a,
+  a:link,
+  a:visited,
+  a:hover,
+  a:active {
+    text-decoration: none;
+  }
+  textarea {
+    resize: none;
+  }
+  table{
+    padding:0;
+    border:0;
+    border-spacing:0px;
+    border-collapse:collapse;
+  }
+  th, td{
+    padding:0;
+    font-weight: 400;
+  }
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+  }
+
+`
